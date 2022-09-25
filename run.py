c9a008f614406a5e2c8bb8136f366b5b740e6c56
NickWaldock <nickwaldock@me.com>
2022-09-25 19:53:46 +0000

    give user initial choices at start

--- run.py
+++ run.py
@@ -1,9 +1,27 @@
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
 
-print("Wecome to the Practice Log!")
+print("Wecome to the Practice Log!\n")
 
-def get_name():
+def start():
     username = input("Please enter your name: ")
+    print(f"\nHello {username}! What would you like to do?\n")
+    print("1. Log a practice session")
+    print("2. Get insights on your practice")
+    print("3. Get practice ideas")
+    print("4. Quit\n")
+    print("Make your selection with a number. ie: 1")
+    start_choice = int(input())
 
-get_name()
-    print(f"Hello {username}! What would you like to do?" )
+    if start_choice == 1:
+        print("\n You have chosen to log a new practice session.\n")
+    elif start_choice == 2:
+        print("\n You have chosen to get insights on your logged practice sessions.\n")
+    elif start_choice == 3:
+        print("\nYou have chosen to get practice ideas")
+    elif start_choice == 4:
+        print("\nQuitting, thank you for logging your practice")
+        exit()
+    
+
+start()
+
