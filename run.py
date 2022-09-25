bce0b72cb2461e4f078ba1a6b61450c82eae41b5
NickWaldock <nickwaldock@me.com>
2022-09-25 19:28:55 +0000

    initial commit, create user welcome statement

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+print("Wecome to the Practice Log!")
+
+def get_name():
+    username = input("Please enter your name: ")
+
+get_name()
+    print(f"Hello {username}! What would you like to do?" )
