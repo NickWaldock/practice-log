f97d23b91ebfe387dc949ebc02de918783cd2745
NickWaldock <nickwaldock@me.com>
2022-10-02 13:33:15 +0000

    add function log_topics

--- run.py
+++ run.py
@@ -19,8 +19,8 @@ SHEET = GSPREAD_CLIENT.open("practice_log")
 
 log = SHEET.worksheet("log")
 
-data = log.get_all_values()
-print(data)
+# data = log.get_all_values()
+# print(data)
 
 def log_practice():
     """
@@ -45,7 +45,7 @@ def log_date():
         print(f"\nToday's date is {session_date}\n")
 
     elif session_today.lower() == "n":
-        # Validate for correct date input format
+        # Validate for correct date input format (Code Ref.1)
         while True:
             try:
                 session_date = input("\nPlease input the date of your practice session (DD/MM/YY):\n")
@@ -61,7 +61,7 @@ def log_date():
 
         if valid_date is True:
             print(f"\nYou practiced on {session_date}\n")
-    time.sleep(2)
+    time.sleep(1.5)
 
 
 def log_duration():
@@ -77,7 +77,7 @@ def log_duration():
         else:
             print(f"\nWell done! You practiced for {session_duration} mins!")
             break
-    time.sleep(2)
+    time.sleep(1.5)
 
 
 def log_score():
@@ -88,12 +88,36 @@ def log_score():
     prod_score = int(input("\nOn a scale of 1 - 10, how productive do you feel the session was?\n"))
     if prod_score <= 3:
         print(f"\n{prod_score} is ok, you still practiced!\nIt's the consistency that counts\n")
+        time.sleep(1.5)
     elif prod_score <= 5:
         print(f"\n{prod_score} is a good score! All progress is good progress!\n")
+        time.sleep(1.5)
     elif prod_score <= 8:
         print(f"\n{prod_score} is fantastic! Well done!\n")
+        time.sleep(1.5)
     elif prod_score <= 10:
         print(f"\n{prod_score} is awesome! You are smashing it!\n")
+        time.sleep(1.5)
+
+
+def log_topics():
+    """
+    Ask user to log the general topics they covered in their practice, and
+    explain to the user the four general topics of practice they should refer to
+    """
+    print("In this model, general practice topics are classed as either: Technical, Musicianship, Creative, or Repertoire")
+    print("\nWhich general practice topics did you work on in this practice session?")
+    topics_log = []
+
+    while True:
+        try:
+            topics_log = input('\n(Please seperate your choices with a ",")\n')
+        except ValueError:
+            print('\nPlease enter a selection of the above topics seperated with a ","\n')
+        else:
+            print("\nThat's great! I've made a note of that")
+            break
+    time.sleep(1.5)
 
 def start():
     """
@@ -112,6 +136,7 @@ def start():
         log_date()
         log_duration()
         log_score()
+        log_topics()
     elif start_choice == 2:
         print("\n You have chosen to get insights on your logged practice sessions\n")
     elif start_choice == 3:
@@ -123,5 +148,5 @@ def start():
 
 print("\n ** Wecome to the Practice Log! **\n")
 print("What would you like to do?\n")
-# start()
+start()
 
